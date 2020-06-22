89d45f90cf001fd6e16134e6dcfd9481a6e3f75d
diana magno <dianamagno@gmail.com>
2020-06-23 07:23:46 +0800

    update for admin accout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                     </div>
                     </div>
                     <ul class="navbar-nav ml-lg-auto color-black">
-                        @if(Auth::user()->isAdmin == 'Y')
+                        {{-- @if(Auth::user()->isAdmin == 'Y')
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/users')}}">Users
                                 <svg class="bi bi-person" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -78,7 +78,7 @@
                         </li>
                         <div class="dropdown-divider"></div>
                         @else
-                        @endif
+                        @endif --}}
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/staff')}}">Staff
                                 <svg class="bi bi-person-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -115,11 +115,26 @@
                             @if(Auth::user()->isAdmin == 'Y')
                             <li class="nav-item">
                                 <a class="dropdown-item" href="{{ url('/designation')}}">Designation
+                                    <svg class="bi bi-person" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                    </svg>
                                 </a>
                             </li>
                             <div class="dropdown-divider"></div>
                             <li class="nav-item">
                                     <a class="dropdown-item" href="{{ url('/type')}}">Equipment Type
+                                        <svg class="bi bi-laptop" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M13.5 3h-11a.5.5 0 0 0-.5.5V11h12V3.5a.5.5 0 0 0-.5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11z"/>
+                                            <path d="M0 12h16v.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5V12z"/>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <div class="dropdown-divider"></div>
+                                <li class="nav-itemdropdown-item">
+                                    <a class="dropdown-item" href="{{ url('/users')}}">Users
+                                        <svg class="bi bi-person" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                        </svg>
                                     </a>
                                 </li>
                                 <div class="dropdown-divider"></div>
